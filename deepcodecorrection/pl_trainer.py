a27772ac703cf5bdd21827806c05244b9d350c73
Adrien B <forbu14@gmail.com>
2024-01-28 12:50:30 +0100

    final (non tested) code

--- deepcodecorrection/pl_trainer.py
+++ deepcodecorrection/pl_trainer.py
@@ -6,18 +6,24 @@ import torch
 import torch.nn as nn
 import lightning.pytorch as pl
 
+import torchmetrics
+
+from vector_quantize_pytorch import FSQ
+
 
 class PLTrainer(pl.Trainer):
     """
     Class for training with PyTorch Lightning
     """
 
-    def __init__(self, dim_global, max_dim_input, nb_class):
+    def __init__(self, max_dim_input, nb_class, dim_global=32, coeff_code_rate=1.3):
         super().__init__()
 
         self.dim_global = dim_global
         self.max_dim_input = max_dim_input
         self.nb_class = nb_class
+        self.dim_global = dim_global
+        self.coeff_code_rate = coeff_code_rate
 
         self.emitter_transformer = nn.Transformer(
             d_model=dim_global,
@@ -55,9 +61,21 @@ class PLTrainer(pl.Trainer):
         # embedding for the actual input
         self.input_embedding_emitter = nn.Embedding(nb_class, dim_global)
 
-        self.criterion = nn.CrossEntropyLoss()
+        # FSQ
+        levels = [8]  # see 4.1 and A.4.1 in the paper
+        self.quantizer = FSQ(levels)
 
-    def forward(self, x, coeff_code_rate):
+        # three resizing components
+        self.resize_emitter = nn.Linear(dim_global, 1)
+        self.resize_receiver = nn.Linear(1, dim_global)
+        self.resize_output = nn.Linear(dim_global, nb_class)
+
+        self.criterion = nn.CrossEntropyLoss()
+        
+        # accuracy metric
+        self.accuracy = torchmetrics.Accuracy()
+
+    def forward(self, x, coeff_code_rate, noise_level):
         """
         Forward pass of the neural network model.
 
@@ -79,14 +97,14 @@ class PLTrainer(pl.Trainer):
         # generate embedding for the input
         x = self.input_embedding_emitter(x)  # batch_size, seq_length, dim_global
 
-        x_position = (
+        emitter_position = (
             torch.arange(seq_length).unsqueeze(0).repeat(batch_size, 1).to(x.device)
         )
-        x_position = self.position_embedding_encoder_emitter(
-            x_position
+        emitter_position = self.position_embedding_encoder_emitter(
+            emitter_position
         )  # batch_size, seq_length, dim_global
 
-        x = x + x_position
+        x = x + emitter_position
 
         # now we generate embedding for the emiiter decoder and the receiver encoder
         transmitter_position = (
@@ -108,19 +126,80 @@ class PLTrainer(pl.Trainer):
             x, transmitter_position
         )  # batch_size, dim_intermediate, dim_global
 
-        # discretization with FSQ
-        # TODO
+        # resize to batch_size, dim_intermediate, 1
+        transmitted_information = self.resize_emitter(transmitted_information)
 
         # adding noise
-        # TODO
-        received_information = transmitted_information # dim batch_size, dim_intermediate, 1
+        transmitted_information = (
+            transmitted_information
+            + torch.randn_like(transmitted_information) * noise_level
+        )
 
-        # TODO resize to global dimension
-        received_information = received_information
+        # discretization with FSQ
+        received_information, _ = self.quantizer(transmitted_information)
+
+        # resize to global dimension
+        received_information = self.resize_receiver(received_information)
+
+        # adding position embedding
+        received_information = received_information + receiver_position
 
         # second the receiver transformation
-        output = self.receiver_transformer(received_information, receiver_position)
-        
+        output = self.receiver_transformer(received_information, emitter_position)
+
         # final resizing to output logits
+        output = self.resize_output(output)
 
         return output
+
+    def compute_loss(self, x, coeff_code_rate, noise_level):
+        """
+        Compute the loss function.
+
+        Args:
+            self: the neural network instance
+            x: input tensor of shape (batch_size, seq_length)
+            coeff_code_rate: coefficient for code rate adjustment
+
+        Returns:
+            loss: loss value
+            output: tensor representing the output of the neural network (batch_size, seq_length, nb_class)
+        """
+        output = self.forward(x, coeff_code_rate, noise_level)
+
+        # loss (cross entropy)
+        loss = self.criterion(output, x)
+
+        # accuracy
+        accuracy = self.accuracy(output, x)
+
+        return loss, output, accuracy
+
+    def training_step(self, batch, batch_idx):
+        """
+        Perform a training step for the given batch and batch index.
+
+        Args:
+            batch: The input batch for training.
+            batch_idx: The index of the batch.
+
+        Returns:
+            The loss value after the training step.
+        """
+        x = batch
+
+        batch_size, seq_lenght = x.shape
+
+        # choose random noise level
+        noise_level = 0.2
+
+        # choose random code rate
+        coeff_code_rate = self.coeff_code_rate
+
+        loss, _, accuracy = self.compute_loss(x, coeff_code_rate, noise_level)
+
+        self.log("train_loss", loss)
+        self.log("train_accuracy", accuracy)
+
+        return loss
+    
